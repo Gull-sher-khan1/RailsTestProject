9385839cb4624fc01aa703d2717930614c447071
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-05-31 16:53:53 +0500

    Post model generated
    *migrations: appropriate required columns added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :followings
+  has_many :posts
+  has_many :stories
   has_one :request, class_name: "following", foreign_key: "follower_id"
 end

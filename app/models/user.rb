0735ff2849d8ef3574614f9a1c36eac5cf0da824
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-05-31 16:43:55 +0500

    Following model generated
    *Migration: proper Foreign keys and required information column added and run

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :followings
+  has_one :request, class_name: "following", foreign_key: "follower_id"
 end

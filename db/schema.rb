489e49dc98ec324f2bf499eb2a8c0e1ca28ad9cd
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-05-31 17:01:07 +0500

    comments model generated
    *migrations: appropriate columns added
    *Associations: added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_115234) do
+ActiveRecord::Schema.define(version: 2022_05_31_115639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "commentable_id"
+    t.string "commentable_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+  end
 
   create_table "followings", force: :cascade do |t|
     t.boolean "requestAccepted"

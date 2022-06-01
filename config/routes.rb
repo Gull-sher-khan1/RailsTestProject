df5bb34341fda4fb7937d9e5af527e82a5ac8567
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-06-01 14:40:57 +0500

    Home controller added
    *index view added

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
     resources :likes, only: [:destroy, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "welcome#index"
+  root to: "home#index"
 end

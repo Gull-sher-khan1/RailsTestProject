e45e2020fe41b490ece5d06ac3d28627f7306d03
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-06-07 17:39:15 +0500

    comment update done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,19 @@ class CommentsController < ApplicationController
       format.js {render 'home/remove_comment.js.erb', layout: false, locals: {comment_id: params[:id]}}
     end
   end
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(text: params[:comment][:text])
+    respond_to do |format|
+      format.js {render 'home/edit_comment.js.erb', layout: false, locals: {comment: @comment}}
+    end
+  end
+  def edit
+    @comment =  Comment.find(params[:id])
+    respond_to do |format|
+      format.js {render 'home/edit_comment.js.erb', layout: false, locals: {comment: @comment}}
+    end
+  end
   private
   def strong_params
     params.permit!

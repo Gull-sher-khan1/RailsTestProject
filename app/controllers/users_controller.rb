676354399b440657227723688a2e51284cb3b934
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-06-08 17:18:08 +0500

    User profile name edit done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,22 @@ class UsersController < ApplicationController
     @likes_posts = Like.includes(:user).where(likeable_type: "Post")
   end
   def edit
+    @user=User.find(params[:id])
+    respond_to do |format|
+      format.js {render 'edit_name_form.js.erb', layout: false, locals:{user: @user}}
+    end
   end
   def update
     @user=User.find(params[:id])
-    puts params[:from]
     if params[:from] == "private"
       @user.update(private_account: @user.private_account!=true ? true : false)
       respond_to do |format|
         format.js {render 'change_private_button.js.erb', layout: false}
+      end
+    else
+      @user.update(first_name: params[:user][:first_name], last_name: params[:user][:last_name])
+      respond_to do |format|
+        format.js {render 'edit_name_form.js.erb', layout: false, locals: {user: @user}}
       end
     end
   end

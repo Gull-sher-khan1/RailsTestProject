4510129d706248ba5b5c62179a333a3fc60b4f40
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-06-08 16:04:59 +0500

    private account option done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,23 @@ class UsersController < ApplicationController
   layout 'navbar'
   def show
     @user=User.find(params[:id])
-    @posts = Post.where(user_id: @user.id)
+    if @user.id == current_user.id || (@user.id != current_user.id && @user.private_account!=true)
+      @posts = Post.where(user_id: @user.id)
+    else
+      @posts = nil
+    end
     @likes_posts = Like.includes(:user).where(likeable_type: "Post")
+  end
+  def edit
+  end
+  def update
+    @user=User.find(params[:id])
+    puts params[:from]
+    if params[:from] == "private"
+      @user.update(private_account: @user.private_account!=true ? true : false)
+      respond_to do |format|
+        format.js {render 'change_private_button.js.erb', layout: false}
+      end
+    end
   end
 end

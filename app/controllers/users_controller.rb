d6979b099e5e393776cb9eb04b23e3a2df6dc911
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-06-08 14:47:57 +0500

    user profile posts added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   layout 'navbar'
   def show
     @user=User.find(params[:id])
+    @posts = Post.where(user_id: @user.id)
+    @likes_posts = Like.includes(:user).where(likeable_type: "Post")
   end
 end

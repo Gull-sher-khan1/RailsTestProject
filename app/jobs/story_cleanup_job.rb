f47def6050f35f607decb0c88c1703242929cc7f
Gull-sher-khan1 <gullsher.khan@devsinc.com>
2022-06-15 17:27:09 +0500

    active job story done

---
+++ app/jobs/story_cleanup_job.rb
@@ -0,0 +1,10 @@
+class StoryCleanupJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    uri = args[0].uri.split('/',-1)
+    Cloudinary::Uploader.destroy('rails_test_project/' + uri[uri.size-1].split('.',-1)[0])
+    args[0].destroy
+    args[1].destroy
+  end
+end

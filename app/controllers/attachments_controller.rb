d5ba9d69ddbeb93a0d67ed2f320768eec628a4a6
Gull-sher-khan1 <103023331+Gull-sher-khan1@users.noreply.github.com>
2022-06-20 20:29:39 +0500

    Cloudinary and attachment controller optimized
    Cloudinary Service Object created
    Attachments controller optimized

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,67 +1,54 @@
 class AttachmentsController < ApplicationController
   def index
-    puts params[:attachments]
+
   end
   def create
     if params.key?('attachment')
+
       if params[:from]=='post'
-        @attachments = params[:attachment][:attachment]
-        @post = Post.find(params[:post_id])
-        @attachments.each do |file|
-          res=Cloudinary::Uploader.upload(file.tempfile,:folder => "rails_test_project/")
-          attachment=@post.attachments.new
-          attachment.uri= res["url"]
-          attachment.save
-        end
+        CloudinaryService.batch_upload(Post.find_by_id(params[:post_id]),params[:attachment][:attachment])
+
       elsif params[:from] == 'story'
         @story = Story.new
         @story.user_id= current_user.id
-        @story.save
-        @attachment = @story.build_attachment
-        attachment = params[:attachment][:attachment]
-        res=Cloudinary::Uploader.upload(attachment.tempfile,:folder => "rails_test_project/")
-        @attachment.uri = res["url"]
-        if @attachment.save
-          StoryCleanupJob.set(wait: 60*60*24).perform_later(@attachment,@story)
+        if @story.save
+          @attachment = @story.build_attachment
+          @attachment.uri = CloudinaryService.upload(params[:attachment][:attachment])
+          if @attachment.save
+            StoryCleanupJob.set(wait: 60*60*24).perform_later(@attachment,@story)
+          end
         end
+
       else
-          user = User.find(params[:user_id])
-          attachment = params[:attachment][:attachment]
-          res=Cloudinary::Uploader.upload(attachment.tempfile,:folder => "rails_test_project/")
-          user.create_attachment(uri: res['url'])
-          respond_to do |format|
-            format.js {render 'users/add_pic.js.erb', layout: false, locals: {uri: res['url']}}
+          user = User.find_by_id(params[:user_id])
+          uri = CloudinaryService.upload(params[:attachment][:attachment])
+          if user.create_attachment(uri: uri)
+            respond_to do |format|
+              format.js {render 'users/add_pic.js.erb', layout: false, locals: {uri: uri}}
+            end
           end
       end
+
     end
   end
   def update
     if params[:from] == 'user'
-      @attachment = Attachment.find(params[:id])
-      attachment = params[:attachment][:attachment]
-      uri = @attachment.uri.split('/',-1)
-      Cloudinary::Uploader.destroy('rails_test_project/' + uri[uri.size-1].split('.',-1)[0])
-      res=Cloudinary::Uploader.upload(attachment.tempfile,:folder => "rails_test_project/")
-      @attachment.update(uri: res['url'])
-      respond_to do |format|
-        format.js {render 'users/add_pic.js.erb', layout: false, locals: {uri: res['url']}}
+      @attachment = Attachment.find_by_id(params[:id])
+      if CloudinaryService.destroy(@attachment);
+        uri=CloudinaryService.upload(params[:attachment][:attachment])
+        if @attachment.update(uri: uri)
+          respond_to do |format|
+            format.js {render 'users/add_pic.js.erb', layout: false, locals: {uri: uri}}
+          end
+        end
       end
+
     else
       @attachments = Attachment.where(attachable_id: params[:id], attachable_type: 'Post')
-      @attachments.each do |file|
-        uri = file.uri.split('/',-1)
-        Cloudinary::Uploader.destroy('rails_test_project/' + uri[uri.size-1].split('.',-1)[0])
-        file.destroy
-      end
+      CloudinaryService.batch_destroy(@attachments)
       if params.key?('attachment')
-        @attachments = params[:attachment][:attachment]
         @post = Post.find(params[:id])
-        @attachments.each do |file|
-          res=Cloudinary::Uploader.upload(file.tempfile,:folder => "rails_test_project/")
-          attachment=@post.attachments.new
-          attachment.uri= res["url"]
-          attachment.save
-        end
+        CloudinaryService.batch_upload(@post,params[:attachment][:attachment])
         redirect_back(fallback_location: root_path)
       end
     end

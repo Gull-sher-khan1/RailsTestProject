8e7a54a51789541216c4e5768736190c0b52ed5d
Gull-sher-khan1 <103023331+Gull-sher-khan1@users.noreply.github.com>
2022-06-20 21:00:18 +0500

    Comments Controller optimized

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,40 @@
 class CommentsController < ApplicationController
+
   def create
-    post=Post.find_by_id(params[:comment][:commentable_id])
-    @comment=post.comments.new(strong_params[:comment])
-    @comment.user_id=strong_params[:user_id]
-    @comment.save
-    @user_ids = [current_user.id]
-    @users = User.find(@user_ids)
-    respond_to do |format|
-      format.js {render 'home/render_comment.js.erb', layout: false, locals: {post: post}}
+    post=Post.find_by_id(strong_params[:comment][:commentable_id])
+    @comment=post.comments.create(user_id: strong_params[:user_id], text: strong_params[:comment][:text])
+    if @comment
+      @user_ids = [current_user.id]
+      @users = User.find(@user_ids)
+      respond_to do |format|
+        format.js {render 'home/render_comment.js.erb', layout: false, locals: {post: post}}
+      end
     end
   end
+
   def destroy
-    @comment=Comment.find(params[:id])
-    p @comment
+    @comment=Comment.find_by_id(strong_params[:id])
     @comment.destroy
     respond_to do |format|
       format.js {render 'home/remove_comment.js.erb', layout: false, locals: {comment_id: params[:id]}}
     end
   end
+
   def update
-    @comment = Comment.find(params[:id])
+    @comment = Comment.find_by_id(strong_params[:id])
     @comment.update(text: params[:comment][:text])
     respond_to do |format|
       format.js {render 'home/edit_comment.js.erb', layout: false, locals: {comment: @comment}}
     end
   end
+
   def edit
-    @comment =  Comment.find(params[:id])
+    @comment =  Comment.find_by_id(strong_params[:id])
     respond_to do |format|
       format.js {render 'home/edit_comment.js.erb', layout: false, locals: {comment: @comment}}
     end
   end
+
   private
   def strong_params
     params.permit!

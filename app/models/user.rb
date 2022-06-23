23727cefd8f5cd2644fdc7cd8af0d7337e441c6d
Gull-sher-khan1 <103023331+Gull-sher-khan1@users.noreply.github.com>
2022-06-23 19:59:22 +0500

    user email validations: added

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :last_name, presence: true
   validates_format_of :email,
-  :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  :with => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   protected
   def confirmation_required?

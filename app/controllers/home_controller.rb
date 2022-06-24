085a5af87c461e3c50d4863ef183063b4f9765ee
Gull-sher-khan1 <103023331+Gull-sher-khan1@users.noreply.github.com>
2022-06-24 19:52:55 +0500

    Home controller cleaned
    * Actions length cut down
    * Call backs added
    * Error handling
    * Helper added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
   include UserConcern
   before_action :set_search, :set_comment, :set_posts, :set_likes, :set_stories, :set_attachments, only: [:index, :search]
   before_action :set_post, only: :get_comments
+  before_action :set_attachment, :set_story, only: :show_story
   def index
   end
 
@@ -18,14 +19,18 @@ class HomeController < ApplicationController
     end
   end
 
+  #show story when specific story is clicked
   def show_story
-    @attachment = Attachment.find(strong_params[:id])
-    @story = Story.find_by(id: @attachment.attachable_id)
-    respond_to do |format|
-      format.js { render 'home/render_story.js.erb', layout: false }
+    if @attachment == nil || @story == nil
+      redirect_to helpers.flash_helper('could not find story, try reloading', :alert)
+    else
+      respond_to do |format|
+        format.js { render 'home/render_story.js.erb', layout: false }
+      end
     end
   end
 
+  #search and show result when user tries to search users registered in app
   def search
     @found_users = @search_query.result(distinct: true)
     respond_to do |format|
@@ -35,7 +40,7 @@ class HomeController < ApplicationController
 
   private
   def strong_params
-    params.permit(:commentable_id)
+    params.permit(:commentable_id, :id, :authenticity_token, :q)
   end
 
   def set_comment
@@ -63,10 +68,16 @@ class HomeController < ApplicationController
 
   def set_post
     @post = Post.find_by_id(strong_params[:commentable_id])
-    if @post==nil
-      flash[:alert] = 'could not retrieve comments, try reloading'
-      flash.keep
-      redirect_to root_url
-    end
+    redirect_to helpers.flash_helper('could not retrieve comments, try reloading', :alert) if @post==nil
+  end
+
+  def set_attachment
+    @attachment = Attachment.get_attachment(strong_params[:id])
+    @attachment=nil if @attachment.class.to_s == 'Attachment::ActiveRecord_Relation' && @attachment.count>1
+  end
+
+  def set_story
+    @story = Story.get_story(@attachment==nil ? 0 : @attachment.attachable_id)
+    @story=nil if @attachment.class.to_s == 'Attachment::ActiveRecord_Relation' && @story.count>1
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_helper(msg,name)
+    flash[name] = msg
+    flash.keep
+    root_url
+  end
 end

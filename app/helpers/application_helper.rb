0ea39291555324699850483f5b820d10bd692cec
Gull-sher-khan1 <103023331+Gull-sher-khan1@users.noreply.github.com>
2022-06-27 17:49:38 +0500

    queries removed from views 50%
    * user show
    * navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def request_count(id)
+    Following.request_count(id)
+  end
 
+  def user_attachment(id)
+    Attachment.users_attachments([id])
+  end
 end

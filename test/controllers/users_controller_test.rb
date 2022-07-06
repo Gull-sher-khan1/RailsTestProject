d5cfb017d7552864a3d23f9332ab472fa0b91987
Gull-sher-khan1 <103023331+Gull-sher-khan1@users.noreply.github.com>
2022-07-06 21:37:08 +0500

    user controller edit test done

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  setup do
+    Rails.application.load_seed
+    sign_in User.create(email: 'gullsher.khan@devsinc.com', password: '123456', password_confirmation: '123456', first_name: 'gull sher', last_name: 'khan')
+    @post = Post.last
+    @user = User.last
+  end
+
+  test "show edit form" do
+    get edit_user_url(User.last.id), xhr: true
+    assert_response :success
+    assert_not_equal 'can not find user', flash[:alert]
+  end
+
+  test "dont show edit form" do
+    get edit_user_url(0), xhr: true
+    assert_equal 'can not find user', flash[:alert]
+  end
 end

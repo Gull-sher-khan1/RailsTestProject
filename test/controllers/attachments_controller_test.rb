f09dbce00177a1494b2b374ecf2f13e969602e7d
Gull-sher-khan1 <103023331+Gull-sher-khan1@users.noreply.github.com>
2022-07-07 19:14:20 +0500

    attachments controller test started

--- test/controllers/attachments_controller_test.rb
+++ test/controllers/attachments_controller_test.rb
@@ -3,7 +3,10 @@
 require 'test_helper'
 
 class AttachmentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Rails.application.load_seed
+    sign_in User.create(email: 'gullsher.khan@devsinc.com', password: '123456', password_confirmation: '123456', first_name: 'gull sher', last_name: 'khan')
+    @post = Post.last
+    @user = User.last
+  end
 end

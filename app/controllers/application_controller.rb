07f614c8588c5508ea6e3e6035ecc9a0e01a9be5
Gull-sher-khan1 <103023331+Gull-sher-khan1@users.noreply.github.com>
2022-07-13 15:06:38 +0500

    rails development server fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :redirect_if_not_signed_in, unless: :user_signed_in?
   rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_token
 
   private
 
   def invalid_token
     render file: 'public/invalid'
-  end
-
-  def redirect_if_not_signed_in
-    redirect_to new_user_session_url, alert: 'Please sign in!'
   end
 end
